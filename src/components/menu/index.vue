ea2521e3df2bf3dfe784d16c5de10f3d49d0bbea
LiangQinghai <liangqinghai@jiumaojiu.com>
2023-02-14 23:24:26 +0800

    :construction: menu

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -1,19 +1,31 @@
 <template>
-	<a-menu> </a-menu>
+	<a-menu
+		:mode="topMenu.value ? 'horizontal' : 'vertical'"
+		:style="{ width: '100%', height: '100%' }"
+		:show-collapse-button="appStore.device !== 'mobile'"
+		:auto-open="false"
+		:level-indent="34"
+		:onCollapse="setCollapse"
+	>
+		<a-menu-item key="1">Nav1</a-menu-item>
+		<a-sub-menu key="2">
+			<template #title>Nav2</template>
+			<a-menu-item key="2_1">sub01</a-menu-item>
+			<a-menu-item key="2_2">sub02</a-menu-item>
+			<a-menu-item key="2_3">sub03</a-menu-item>
+		</a-sub-menu>
+	</a-menu>
 </template>
-<script lang="ts"></script>
+<script lang="ts" setup>
+import { useAppStore } from "@/store";
+import { computed } from "vue";
 
-<style lang="less" scoped>
-:deep(.arco-menu-inner) {
-	.arco-menu-inline-header {
-		display: flex;
-		align-items: center;
-	}
+const appStore = useAppStore();
+const topMenu = computed(() => appStore.topMenu);
 
-	.arco-icon {
-		&:not(.arco-icon-down) {
-			font-size: 18px;
-		}
-	}
-}
-</style>
+const setCollapse = (val: boolean) => {
+	if (appStore.device === "desktop") appStore.updateSettings({ menuCollapse: val });
+};
+</script>
+
+<style lang="less" scoped></style>
